46f196382efb3389d0e4b4867fba1e814e1db02e
Lipev-dev <filipeveira19@gmail.com>
2025-03-27 18:47:44 +0000

    Eu criei toda a estrutura do jogo CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -43,6 +43,39 @@ int main() {
 
     printf("Existem %d pontos turísticos \n", pontos_turisticos);
     
+    // ---------------------------------------------------------------------------------------------------------------------------
+
+
+    printf("Digite o codigo da cidade: ");
+    scanf("%s", &codigo_cidade);
+    
+    printf("Digite o nome da cidade: ");
+    scanf("%s", &nome_cidade);
+    
+    printf("Digite a população da cidade: ");
+    scanf("%d", &populacao);
+    
+    printf("Digite a área da cidade: ");
+    scanf("%f", &area);
+    
+    printf("Digite o pib da cidade: ");
+    scanf("%f", &pib);
+    
+    printf("Digite quantos pontos turtistico existem: ");
+    scanf("%d", &pontos_turisticos);
+    
+    printf("O código da cidade é: %s \n", codigo_cidade);
+    
+    printf("O nome da cidade é: %s \n", nome_cidade);
+    
+    printf("A população é de: %d \n", populacao);
+    
+    printf("O pib é de: %f \n", pib);
+     
+    printf("A área é: %f \n", area);
+    
+    printf("Existem %d pontos turísticos \n", pontos_turisticos);
+        
 
    
     // Cadastro das Cartas:
